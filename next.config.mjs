a329c2be777f658e8013a83004131d9624374fa8
Anas <anasemadkarab@gmail.com>
2026-09-14 14:39:47 +0300

    Rename next.config to mjs for Next 14 compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     // هاد السطر بيطفي تدقيق الأخطاء الإملائية للكود وقت الرفع
     ignoreDuringBuilds: true,
